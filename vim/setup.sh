d3a5cb4dbf8c1b1fe6e17a25d6977cc0601d34ac
Robert McBride <robmcbride@gmail.com>
2013-03-18 20:53:11 -0700

    syncing what plugins I use at work

--- vim/setup.sh
+++ vim/setup.sh
@@ -29,56 +29,63 @@ mkdir bundle
 cd bundle
 
 # now git
-git clone http://github.com/tpope/vim-commentary.git
+#git clone http://github.com/tpope/vim-commentary.git
 git clone http://github.com/tpope/vim-repeat.git
 git clone http://github.com/tpope/vim-endwise.git
-git clone http://github.com/tpope/vim-rails.git
+#git clone http://github.com/tpope/vim-rails.git
 git clone http://github.com/tpope/vim-surround.git
 git clone http://github.com/tpope/vim-fugitive.git
 git clone http://github.com/scrooloose/nerdtree.git
 git clone http://github.com/scrooloose/syntastic.git
+git clone http://github.com/scrooloose/nerdcommenter.git
 git clone http://github.com/mattn/zencoding-vim.git
 git clone http://github.com/majutsushi/tagbar.git
 git clone http://github.com/kien/ctrlp.vim.git
 git clone http://github.com/mileszs/ack.vim.git
 git clone http://github.com/michaeljsmith/vim-indent-object.git
-git clone http://github.com/vim-scripts/bufkill.vim.git
 git clone http://github.com/vim-scripts/ZoomWin.git
 git clone http://github.com/vim-scripts/matchit.zip.git
-git clone http://github.com/godlygeek/tabular.git
-git clone http://github.com/chrisbra/csv.vim.git
-git clone http://github.com/vim-scripts/vimwiki.git
+#git clone http://github.com/godlygeek/tabular.git
+#git clone http://github.com/vim-scripts/vimwiki.git
 git clone http://github.com/Lokaltog/powerline.git
+#git clone http://github.com/Lokaltog/vim-powerline
+git clone http://github.com/vim-scripts/closetag.vim.git
+git clone http://github.com/dbarsam/vim-bufkill.git
+git clone http://github.com/vim-scripts/YankRing.vim.git
 
 git clone http://github.com/ervandew/supertab.git
+
+# colors
+git clone http://github.com/altercation/vim-colors-solarized.git
 
 # faster movement
 git clone http://github.com/Lokaltog/vim-easymotion.git
 git clone http://github.com/goldfeld/vim-seek.git
 
 # snipmate and deps
-git clone http://github.com/tomtom/tlib_vim.git
-git clone http://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone http://github.com/garbas/vim-snipmate.git
+git clone http://github.com/SirVer/ultisnips.git
+#git clone http://github.com/tomtom/tlib_vim.git
+#git clone http://github.com/MarcWeber/vim-addon-mw-utils.git
+#git clone http://github.com/garbas/vim-snipmate.git
 #git clone http://github.com/honza/snipmate-snippets
 
 git clone http://github.com/vim-scripts/dbext.vim.git
 
 # Plugins to autoclose brackets and what not
-#git clone http://github.com/vim-scripts/AutoClose.git
+#git clone http://github.cmm/vim-scripts/AutoClose.git
 git clone http://github.com/Raimondi/delimitMate.git
 #git clone http://github.com/kana/vim-smartinput.git
 
 # syntax highlighting, formatting, etc.
 git clone http://github.com/vim-ruby/vim-ruby.git
-git clone http://github.com/tpope/vim-markdown.git
-git clone http://github.com/tpope/vim-haml.git
+#git clone http://github.com/tpope/vim-markdown.git
+#git clone http://github.com/tpope/vim-haml.git
 git clone http://github.com/itspriddle/vim-jquery.git
 git clone http://github.com/pangloss/vim-javascript.git
-git clone http://github.com/kchmck/vim-coffee-script.git
-git clone http://github.com/digitaltoad/vim-jade.git
-git clone http://github.com/vim-scripts/nginx.vim.git
-git clone http://github.com/vim-scripts/nxc.vim.git
+#git clone http://github.com/kchmck/vim-coffee-script.git
+#git clone http://github.com/digitaltoad/vim-jade.git
+#git clone http://github.com/vim-scripts/nginx.vim.git
+#git clone http://github.com/vim-scripts/nxc.vim.git
 
 ##############
 # No longer used plugins
@@ -87,13 +94,9 @@ git clone http://github.com/vim-scripts/nxc.vim.git
 
 # possibilities
 # git clone https://github.com/chrisbra/NrrwRgn.git
-# git clone http://github.com/scrooloose/nerdcommenter.git
 
 # open a scratch buffer, just use :enew now
 # git clone http://github.com/duff/vim-scratch.git
-
-# possible alternative to snipmate?
-#git clone http://github.com/SirVer/ultisnips.git
 
 # neet idea, but I haven't ever used it
 #git clone http://github.com/superjudge/tasklist-pathogen.git
@@ -120,26 +123,8 @@ git clone http://github.com/vim-scripts/nxc.vim.git
 #git clone http://github.com/vim-scripts/java_getset.vim.git
 #git clone http://github.com/vim-scripts/javacomplete.git
 
-# latex-box - helpful latex stuff
-#bzr branch lp:vim-latex-box
-
-# slime.vim
-# replaced by vicle above
-#mkdir -p slime/plugin && cd slime/plugin
-#wget http://s3.amazonaws.com/mps/slime.vim -O slime.vim
-#cd -
-
-# get rid of git and bzr directories/files, should improve startup times for pathogen
-#find -type d -name '.git' -exec rm -rf '{}' ';' 2>/dev/null
-#find -type d -name '.bzr' -exec rm -rf '{}' ';' 2>/dev/null
-#find -type f -name '.gitmodules' -exec rm -rf '{}' ';' 2>/dev/null
-#find -type f -name '.gitignore' -exec rm -rf '{}' ';' 2>/dev/null
-
-# get rid of README files, and other files not used by vim
-#find -type f -name 'Rakefile' -exec rm -rf '{}' ';' 2>/dev/null
-#find -type f -name 'Makefile' -exec rm -rf '{}' ';' 2>/dev/null
-#find -type f -name 'README' -exec rm -rf '{}' ';' 2>/dev/null
-#find -type f -name 'README.markdown' -exec rm -rf '{}' ';' 2>/dev/null
+# for editing and formatting csv
+#git clone http://github.com/chrisbra/csv.vim.git
 
 # back to root vim dir
 cd $root 
